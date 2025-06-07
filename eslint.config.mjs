488ce32fe2991203b6fcef6a096d6a133d905ef9
aaimtt <43095136+aaimtt@users.noreply.github.com>
2025-06-07 23:43:20 +0200

    feat: bootstrap next app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 import eslint from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginImport from 'eslint-plugin-import'
@@ -6,27 +9,37 @@ import eslintPluginPrettier from 'eslint-plugin-prettier'
 import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
 import typescriptEslint from 'typescript-eslint'
 
-export default typescriptEslint.config(
-  {
-    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
-  },
-  eslint.configs.recommended,
-  ...typescriptEslint.configs.strict,
-  ...typescriptEslint.configs.stylistic,
-  {
-    plugins: {
-      'no-only-tests': eslintPluginNoOnlyTests,
-      'simple-import-sort': eslintPluginSimpleImportSort,
-      import: eslintPluginImport,
-      prettier: eslintPluginPrettier,
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...typescriptEslint.config(
+    {
+      ignores: ['dist/**', 'node_modules/**', 'coverage/**', 'build/**', 'out/**'],
     },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
-      'no-only-tests/no-only-tests': 'error',
-      'prettier/prettier': 'error',
-      'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': 'error',
+    eslint.configs.recommended,
+    ...typescriptEslint.configs.strict,
+    ...typescriptEslint.configs.stylistic,
+    {
+      plugins: {
+        'no-only-tests': eslintPluginNoOnlyTests,
+        'simple-import-sort': eslintPluginSimpleImportSort,
+        import: eslintPluginImport,
+        prettier: eslintPluginPrettier,
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        'no-only-tests/no-only-tests': 'error',
+        'prettier/prettier': 'error',
+        'simple-import-sort/exports': 'error',
+        'simple-import-sort/imports': 'error',
+      },
     },
-  },
-  eslintConfigPrettier,
-)
+    eslintConfigPrettier,
+  ),
+]
