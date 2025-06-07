db8261d1a1892a925e5a8a761ee57e35303a35c7
aaimtt <43095136+aaimtt@users.noreply.github.com>
2025-06-07 23:07:21 +0200

    chore: setup tools

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginImport from 'eslint-plugin-import'
+import eslintPluginNoOnlyTests from 'eslint-plugin-no-only-tests'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import typescriptEslint from 'typescript-eslint'
+
+export default typescriptEslint.config(
+  eslint.configs.recommended,
+  ...typescriptEslint.configs.strict,
+  ...typescriptEslint.configs.stylistic,
+  {
+    plugins: {
+      'no-only-tests': eslintPluginNoOnlyTests,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      import: eslintPluginImport,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-only-tests/no-only-tests': 'error',
+      'prettier/prettier': 'error',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+    },
+  },
+  eslintConfigPrettier,
+)
